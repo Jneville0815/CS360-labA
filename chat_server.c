c1089f455ba683d28876e883d85e077c093d96d9
Jimmy Neville <jneville0815@yahoo.com>
2020-04-22 16:05:02 -0400

    progress

--- chat_server.c
+++ chat_server.c
@@ -46,24 +46,20 @@ JRB jrb;
 
 int main(int argc, char **argv) {
 	JRB tmp;
-	pthread_t tid;
+	pthread_t *tid;
 	int sock;
 	int fd;
-	pthread_mutex_t lock;
-	pthread_cond_t cond;
 	ChatRoom *chat_room;
 	Client *client;
 
-	if(argc < 4) {
+	if(argc < 3) {
 		fprintf(stderr, "usage: chat_server\n");
 		exit(1);
 	}
 
 	jrb = make_jrb();
-	pthread_mutex_init(&lock, NULL);
-	pthread_cond_init(&cond, NULL);
 
-	sock = serve_socket(atoi(argv[2]));
+	sock = serve_socket(atoi(argv[1]));
 
 	/*  Create an instance of chat_room for each chat room name 
 		specified on the command line. Also initialize/
@@ -71,9 +67,9 @@ int main(int argc, char **argv) {
 		for each chat room.
 	*/
 	pthread_t *chat_room_tids;
-	chat_room_tids = malloc(sizeof(chat_room_tids) * (argc - 3));
+	chat_room_tids = malloc(sizeof(chat_room_tids) * (argc - 2));
 	int i;
-	for(i = 3; i < argc; i++) {
+	for(i = 2; i < argc; i++) {
 		chat_room = (ChatRoom *) malloc(sizeof(ChatRoom));
 		chat_room->room_name = malloc(strlen(argv[i])+1);
 		strcpy(chat_room->room_name, argv[i]);
@@ -84,10 +80,10 @@ int main(int argc, char **argv) {
 		chat_room->messages = new_dllist();
 		chat_room->clients = new_dllist();
 		chat_room->lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-		chat_room->lock = &lock;
 		chat_room->cond = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
-		chat_room->cond = &cond;
-		pthread_create(chat_room_tids+(i-3), NULL, chat_room_thread, chat_room);
+		pthread_mutex_init(chat_room->lock, NULL);
+		pthread_cond_init(chat_room->cond, NULL);
+		pthread_create(chat_room_tids+(i-2), NULL, chat_room_thread, chat_room);
 	}
 
 
@@ -100,9 +96,9 @@ int main(int argc, char **argv) {
 		fd = accept_connection(sock);
 		client = (Client *) malloc(sizeof(Client));
 		client->fd = fd;
-		pthread_create(&tid, NULL, client_thread, client);
+		tid = malloc(sizeof(pthread_t));
+		pthread_create(tid, NULL, client_thread, client);
 	}
-	pthread_exit(NULL);
 
     return 0;
 }
@@ -111,26 +107,39 @@ void *chat_room_thread(void *arg) {
 	ChatRoom *chat_room;
 	chat_room = (ChatRoom *) arg;
 
+	// one loop while(1)
+	// gets msg, traverse list, send msg to everyone
+	// sits there and sends messages when it gets one
 	return NULL;
 }
 
 void *client_thread(void *arg) {
 	Client *client;
 	ChatRoom *chat_room;
-	JRB tmp;
+	JRB tmp_j;
+	Dllist tmp_d;
 	client = (Client *) arg;
 	client->client_name = malloc(50);
+
+	char *buf = malloc(500);
 
 	char *chat_room_name = malloc(100);
 
 	client->fin = fdopen(client->fd, "r");
 	client->fout = fdopen(client->fd, "w");
 
+	Client *tmp_client;
 	fputs("Chat Rooms:\n\n", client->fout);
-	jrb_traverse(tmp, jrb) {
-		chat_room = (ChatRoom *) tmp->val.v;
+	jrb_traverse(tmp_j, jrb) {
+		chat_room = (ChatRoom *) tmp_j->val.v;
 		fputs(chat_room->room_name, client->fout);
-		fputs(":\n", client->fout);
+		fputs(": ", client->fout);
+		dll_traverse(tmp_d, chat_room->clients) {
+			tmp_client = (Client *) tmp_d->val.v;
+			fputs(tmp_client->client_name, client->fout);
+			fputs(" ", client->fout);
+		}
+		fputs("\n", client->fout);
 	}
 	fputs("\n", client->fout);
 	fflush(client->fout);
@@ -138,15 +147,26 @@ void *client_thread(void *arg) {
 	fputs("Enter your chat name (no spaces):\n", client->fout);
 	fflush(client->fout);
 	fgets(client->client_name, 50, client->fin);
+	client->client_name[strlen(client->client_name) - 1] = 0;
 
 	fputs("Enter chat room:\n", client->fout);
 	fflush(client->fout);
 	fgets(chat_room_name, 100, client->fin);
+	chat_room_name[strlen(chat_room_name) - 1] = 0;
 
-	jrb_traverse(tmp, jrb) {
-		chat_room = (ChatRoom *) tmp->val.v;
-		if(strncmp(chat_room->room_name, chat_room_name, strlen(chat_room->room_name)) == 0) {
-			
+	jrb_traverse(tmp_j, jrb) {
+		chat_room = (ChatRoom *) tmp_j->val.v;
+		if(strcmp(chat_room->room_name, chat_room_name) == 0) {
+			sprintf(buf, "%s has joined\n", client->client_name);
+
+			pthread_mutex_lock(chat_room->lock);
+			// hold on to chat room
+			// access chat_room list of clients
+			// append to list
+			dll_append(chat_room->clients, new_jval_v((void *) client));
+			pthread_cond_signal(chat_room->cond);
+			pthread_mutex_unlock(chat_room->lock);
+			break;
 		}
 	}
 
